4397d4d96098d58fe907856f88a5711c1c4c8b16
chaeh <chaehee15@naver.com>
2025-03-23 23:05:47 +0900

    실습과제 완료

--- 14_LEECHAEHEE/01_DQL_QUESTION.sql
+++ 14_LEECHAEHEE/01_DQL_QUESTION.sql
@@ -1,5 +1,6 @@
 use empdb;
 -- 1. employee 테이블에서 이름 끝이 연으로 끝나는 사원의 이름을 출력하시오
+
 /*
     출력
 
@@ -7,6 +8,12 @@ use empdb;
     전지연
 
 */
+SELECT
+    EMP_NAME
+FROM
+    employee
+WHERE
+    EMP_NAME LIKE '%연';
 
 -- 2. employee 테이블에서 전화번호 처음 3자리가 010이 아닌 사원의 이름, 전화번호를 출력하시오
 
@@ -17,7 +24,13 @@ use empdb;
     하동운 01158456632
 
 */
-
+SELECT
+      EMP_NAME
+    , PHONE
+FROM
+      employee
+WHERE
+       PHONE NOT LIKE '010%';
 -- 3. employee 테이블에서 메일주소 '_'의 앞이 4자이면서, DEPT_CODE가 D9 또는 D5이고 고용일이 90/01/01 ~ 01/12/31이면서, 월급이 270만원 이상인 사원의 전체 정보를 출력하시오
 
 /*
@@ -26,6 +39,18 @@ use empdb;
 
 
 */
+SELECT
+      *
+FROM
+    employee
+WHERE
+    EMAIL LIKE '____\_%'
+AND
+    (DEPT_CODE = 'D9' OR DEPT_CODE = 'D5')
+AND
+    (HIRE_DATE BETWEEN '90/01/01' and '01/12/31')
+AND
+    SALARY >= 2700000;
 
 -- 4. employee테이블에서 현재 근무중인 사원을 이름 오름차순으로 정렬해서 출력.
 
@@ -36,7 +61,14 @@ use empdb;
     ...
 
 */
-
+SELECT
+      EMP_NAME
+  FROM
+      employee
+ WHERE
+      QUIT_YN = 'N'
+ORDER BY
+      EMP_NAME;
 
 
 -- 5. 사원별 입사일, 퇴사일, 근무기간(일)을 조회하세요. 퇴사자 역시 조회되어야 합니다.
@@ -49,10 +81,16 @@ use empdb;
     ...
 
 */
+SELECT
+      EMP_NAME
+    , HIRE_DATE
+    , QUIT_DATE
+    , DATEDIFF(IFNULL(QUIT_DATE, NOW()), HIRE_DATE) AS '근무기간(일)'
+FROM
+    employee;
 
-
--- 6. 재직 중이고 휴대폰 마지막 자리가 2인 직원 중 입사일이 가장 최근인 직원 3명의 사원번호,
--- 직원명, 전화번호, 입사일, 퇴직여부를 출력하세요.
+-- 6. 재직 중이고 휴대폰 마지막 자리가 2인 직원 중 입사일이 가장 최근인 직원 3명의
+-- 사원번호, 직원명, 전화번호, 입사일, 퇴직여부를 출력하세요.
 -- 참고. 퇴사한 직원은 퇴직여부 컬럼값이 ‘Y’이고, 재직 중인 직원의 퇴직여부 컬럼값은 'N'
 
     /*
@@ -62,7 +100,16 @@ use empdb;
         206	박나라	01096935222	2008-04-02 00:00:00	N
     */
 
-
+SELECT
+      EMP_ID, EMP_NAME, PHONE, HIRE_DATE, QUIT_YN
+FROM
+      employee
+WHERE
+      QUIT_YN = 'N'
+AND
+      PHONE LIKE '%2'
+ORDER BY
+       HIRE_DATE DESC LIMIT 3;
 
 -- 7. <1단계> 전체 직원 중 연결된 관리자가 있는 직원의 인원을 출력하세요.
 -- 참고. 연결된 관리자가 있다는 것은 관리자사번이 NULL이 아님을 의미함
@@ -70,6 +117,12 @@ use empdb;
 
     /*
         출력
-
         16
     */
+
+SELECT
+      COUNT(*)
+FROM
+      employee
+WHERE
+     MANAGER_ID IS NOT NULL;
